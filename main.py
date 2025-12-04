d04d69e1f2ddc5bf7e87a7a969af0707d19ebc4d
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2025-12-04 15:55:18 +0530

    add EncoderLayer

--- main.py
+++ main.py
@@ -96,3 +96,20 @@ class MultiHeadAttention(nn.Module):
         x = x.transpose(2, 1).contiguous().view(batch_size, -1, self.d_model)
 
         return self.w_o(x)
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self, d_model, num_heads, d_ff, dropout=0.1):
+        super().__init__()
+        self.self_attn = MultiHeadAttention(d_model, num_heads)
+        self.feed_forward = PositionwiseFeedForward(d_model, d_ff)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, mask):
+        attn_output = self.self_attn(x, x, x, mask)
+        x = self.norm1(x + self.dropout(attn_output))
+        ff_output = self.feed_forward(x)
+        x = self.norm2(x + self.dropout(ff_output))
+        return x
