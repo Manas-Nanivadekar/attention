bab2d7bf9b519cb26fa79639a5b9eeadbbefdd74
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2025-12-04 13:52:05 +0530

    add MHA

--- main.py
+++ main.py
@@ -21,3 +21,42 @@ class ScaledDotProductAttention(nn.Module):
         attn = F.softmax(scores, dim=-1) @ value
 
         return attn
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model: int, num_heads: int):
+        super().__init__()
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.head_dim = d_model // num_heads
+
+        assert (
+            self.head_dim * num_heads == d_model
+        ), "Embedding dimension must be divisible by number of heads"
+
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+        self.w_o = nn.Linear(d_model, d_model)
+
+        self.attention = ScaledDotProductAttention()
+
+    def forward(self, query, key, value, mask=None):
+        batch_size = query.size(0)
+        query = self.w_q(query)
+        key = self.w_k(key)
+        value = self.w_v(value)
+
+        query = query.view(batch_size, -1, self.num_heads, self.head_dim).transpose(
+            1, 2
+        )
+        key = key.view(batch_size, -1, self.num_heads, self.head_dim).transpose(1, 2)
+        value = value.view(batch_size, -1, self.num_heads, self.head_dim).transpose(
+            1, 2
+        )
+
+        x = self.attention(query, key, value, mask=mask)
+
+        x = x.transpose(2, 1).contiguous().view(batch_size, -1, self.d_model)
+
+        return self.w_o(x)
