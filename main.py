ac207bbaee65e930aa4f540d4ba4a09861e6556d
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2025-12-04 13:30:21 +0530

    added SHA

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from attention!")
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
 
 
-if __name__ == "__main__":
-    main()
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(
+        self, query: torch.Tensor, key: torch.Tensor, value: torch.Tensor, mask=None
+    ):
+        d_k = query.size(-1)
+        scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+
+        if mask is not None:
+            causal_mask = torch.tril(torch.ones_like(scores))
+            scores = scores.masked_fill(causal_mask == 0, -1e9)
+
+        attn = F.softmax(scores, dim=-1) @ value
+
+        return attn
